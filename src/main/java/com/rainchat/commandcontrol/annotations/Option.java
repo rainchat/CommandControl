a4adeb65c603a5750876a017895982e96920d849
Павел Сидоренко <super.pasha4444@yandex.ru>
2024-06-08 22:11:04 +1000

    1.0

---
+++ src/main/java/com/rainchat/commandcontrol/annotations/Option.java
@@ -0,0 +1,14 @@
+package com.rainchat.commandcontrol.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Option {
+
+    String value() default "";
+
+}
---
+++ src/main/java/com/rainchat/commandcontrol/annotations/TabComplete.java
@@ -0,0 +1,9 @@
+package com.rainchat.commandcontrol.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TabComplete {
+    String[] value();
+}
